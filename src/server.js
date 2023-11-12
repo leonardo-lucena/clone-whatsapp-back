577d7f32f10de7d29c4ee5a960a7a961f87392c9
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-11-12 17:31:16 -0300

    :construction: feat: Create initials classes

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const routes = require('./router')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+app.request.io = io
+
+server.listen(60000)
